0a69719aa6fd77a0416638a13aa1a39b37b61780
Ferchus <fernanda.prestinoni@fce.uncu.edu.ar>
2025-06-19 18:40:24 -0300

    guardando8

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import "./components/css/main.css";
 import MeetReportViewer from "./components/MeetReportViewer";
 import NavMeet from "./components/NavMeet";
-import AsistenciaDocentes from "./components/AsistenciaDocentes";
 
 function App() {
   return (
